1bbdb663aa6bbbeab7243ba0f688b29b9dd52aef
Collin Lowing <cel19@students.uwf.edu>
2022-02-03 14:32:55 -0600

    add LexicalAnalyzer class

---
+++ LexicalAnalyzer.c
@@ -0,0 +1,9 @@
+/***************************************************************
+  Student Name: Collin Lowing
+  File Name: lexicalAnalyzer.c
+  Project 1
+
+  perform lexical analysis on a character string
+***************************************************************/
+
+#include "LexicalAnalyzer.h"
---
+++ LexicalAnalyzer.h
@@ -0,0 +1,15 @@
+/***************************************************************
+  Student Name: Collin Lowing
+  File Name: lexicalAnalyzer.h
+  Project 1
+
+  perform lexical analysis on a character string
+***************************************************************/
+
+#ifndef COP4020_P1_LEXICALANALYZER_H
+#define COP4020_P1_LEXICALANALYZER_H
+
+int lexan();
+
+
+#endif //COP4020_P1_LEXICALANALYZER_H
