ef2a0c674442fe4f4ac6b3afdbdabdad0549995d
Collin Lowing <cel19@students.uwf.edu>
2022-02-06 18:18:28 -0600

    Header for main.c

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/***************************************************************
+  Student Name: Collin Lowing
+  File Name: main.c
+  Project 1
+
+  runs the rest of the program
+***************************************************************/
+
 #include <stdio.h>
 
 int main() {
