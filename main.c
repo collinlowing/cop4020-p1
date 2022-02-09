3855052401ed3ba81cbd065cefe90050a87cd14e
Collin Lowing <cel19@students.uwf.edu>
2022-02-08 22:14:40 -0600

    accept commandline arguments

--- main.c
+++ main.c
@@ -9,7 +9,13 @@
 #include <stdio.h>
 #include "HashTable.h"
 
-int main() {
+int main(int argc, char** argv) {
+
+    printf("You have entered %d arguments\n", argc - 1);
+
+    for (int i = 1; i < argc; i++)
+        printf("%s\n", argv[i]);
+
     HashItem *table[MAX_HASH_SIZE] = {0};
 
     insertItem(table, "Bart", "first");
@@ -18,5 +24,6 @@ int main() {
     deleteItem(table, "Lisa");
     printTable(table);
     deleteTable(table);
+
     return 0;
 }
